d2bf7c669cd47ba5cd98b29ae3fd09b079431005
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2026-04-28 08:08:10 -0300

    Finetunning

--- experiments.py
+++ experiments.py
@@ -11,16 +11,16 @@ dispositivo = 'GPU' if torch.cuda.is_available() else 'CPU'
 
 DIRETORIO_PADRAO = 'D:\\Dropbox\\Projetos\\pessoal\\QNCA\\results\\'
 
-#experiments = base.QNCAGlobalOptimizer(ca_patterns.rules, cobyla.QNCAOptimizerCOBYLA, path = DIRETORIO_PADRAO)
-#experiments.global_training()
-#experiments.fine_tunning()
+experiments = base.QNCAGlobalOptimizer(ca_patterns.rules, cobyla.QNCAOptimizerCOBYLA, path = DIRETORIO_PADRAO)
+experiments.global_training()
+experiments.fine_tunning()
 
-#experiments = base.QNCAGlobalOptimizer(ca_patterns.rules, cma.QNCAOptimizerCMA, path = DIRETORIO_PADRAO)
-#experiments.global_training()
-#experiments.fine_tunning()
+experiments = base.QNCAGlobalOptimizer(ca_patterns.rules, cma.QNCAOptimizerCMA, path = DIRETORIO_PADRAO)
+experiments.global_training()
+experiments.fine_tunning()
 
 experiments = base.QNCAGlobalOptimizer(ca_patterns.rules, ga.QNCAOptimizerGA, path = DIRETORIO_PADRAO, shots = 100)
-#experiments.global_training()
+experiments.global_training()
 experiments.fine_tunning(k = 30)
 
 
